65cc77ba3a992238c8abd89a6fe34e20b9aac48f
zapisovatel <miroslav.babka@email.cz>
2023-11-26 16:49:14 +0100

    Nearly done

---
+++ escrap_fncs.py
@@ -0,0 +1,116 @@
+""" Election Scraper Functions """
+import os
+import requests
+from bs4 import BeautifulSoup
+
+# URL = "https://volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=2&xnumnuts=2102"
+# URL = "https://volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=12&xnumnuts=7103"
+# URL = "https://volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=12&xnumnuts=7105"
+# CSV_FILE = "vysledky_voleb.csv"
+
+# Const.
+EMPTY_HTML = """<!doctype html>\n<html>\n<body>\n<h3>
+Page not found!
+</h3>\n</body>\n</html>
+"""
+URL_VERIFY = "https://volby.cz/pls/ps2017nss"
+EXT_VERIFY = ".csv"
+VILLAGE_URL_BASE = 'https://volby.cz/pls/ps2017nss/'
+SELECTED_COLUMNS = ['ČÍSLO','OBEC','POČET VOLIČŮ','VYDANÉ OBÁLKY','PLATNÉ HLASY']
+
+# Functions
+def get_village_url(election_village_link: str) -> str:
+    """ Compose the URL with election results for specific village"""
+    return f"{VILLAGE_URL_BASE}{election_village_link}"
+
+def get_html_data(url: str) -> str:
+    """ Downloads the requested web page """
+    try:
+        response = requests.get(url, timeout=5)
+        if response.status_code == 200:
+            html_data = response.text
+        else:
+            html_data = EMPTY_HTML
+    except requests.exceptions.Timeout:
+        html_data = EMPTY_HTML
+    return html_data
+
+def get_villages_list(v_url: str) -> []:
+    """ Get full list of vilages as dict """
+    html_soup = BeautifulSoup(get_html_data(v_url), 'html.parser')
+    v_tables = html_soup.find_all('table')
+    result_list = []
+    # Get data from all tables on page
+    for table in (v_tables):
+        for rows in table.find_all('tr'):
+            rows_data = [td.text for td in rows.find_all('td')]
+            v_link = rows.select('td[class="cislo"] a')
+            if len(rows_data):
+                if rows_data[0].isnumeric():
+                    result_list.append([rows_data[0], rows_data[1], v_link[0].get("href")])
+    return result_list
+
+def get_village_results(v_url: str):
+    """ Function is list of headers and list of results for specific village """
+    # Download data and select tables only
+    html_soup = BeautifulSoup(get_html_data(v_url), 'html.parser')
+    v_tables = html_soup.find_all('table')
+    # General data: only number of voters, envelopes issued, valid votes - columns 3, 4, 7
+    village_general_data = []
+    # List of parties and received votes  - take all
+    list_of_parties = []
+    votes_received = []
+    for counter, table in enumerate(v_tables):
+        for row in table.find_all('tr'):
+            if row.find('td'): # Ommit header rows
+                row_data = [
+                    td.text.replace('\xa0','').replace(',','.').replace('-','')
+                    for td in row.find_all('td')
+                ]
+                # First table is including village general data
+                if not counter: 
+                    village_general_data= [row_data[3], row_data[4], row_data[7]]
+                # Other tables contains results of parties
+                else: 
+                    list_of_parties.append(row_data[1])
+                    votes_received.append(row_data[2])
+    # Compose final lists
+    v_headers_list = SELECTED_COLUMNS + list_of_parties
+    v_data_list = village_general_data + votes_received
+    return v_headers_list, v_data_list
+
+def check_input_arguments(argv_list: []) -> bool:
+    """ Returns true if all done well, otherwise false """
+    if len(argv_list) != 3: # Check if two parameters has been given
+        print("- please run this application with arguments : python escraper.py <url> <filename>")
+        return False
+    if argv_list[1].lower().find(URL_VERIFY) < 0: # Check if the URL points the correct way
+        print("- please specify the URL as \""+ URL_VERIFY + '...\"')
+        return False
+    print("- url to scrape:", argv_list[1])
+    print("- target csv file:", argv_list[2])
+    return True
+
+def cls_print(text_to_display: str) -> None:
+    """ Clear the terminal screen and print the text """
+    # For Windows
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+    print(text_to_display)
+
+def check_csv_filename(csv_name: str) -> str:
+    """ If the filename does not include .csv extension it will correct it on return """
+    correct_name = csv_name
+    if csv_name.lower().find(".csv") < 0: # File does not have CSV extension
+        if csv_name.lower().find(".") > 0: # But it has some extension
+            correct_name = csv_name[0:csv_name.find(".")]+".csv"
+        else:
+            correct_name = csv_name+".csv"
+    return correct_name
+
+if __name__ == "__main__":
+    print(check_csv_filename("csv_name"))
+    print(check_csv_filename("csv_name.bak"))
+    print(check_csv_filename("CSV_NAME.CSV"))
--- escraper.py
+++ escraper.py
@@ -1,115 +1,45 @@
 """ Election Scraper """
-import os
-import requests
+import sys
 import pandas as pd
-from bs4 import BeautifulSoup
+# Functions from escrap_fncs.py
+from escrap_fncs import get_villages_list
+from escrap_fncs import get_village_url
+from escrap_fncs import check_input_arguments
+from escrap_fncs import get_village_results
+from escrap_fncs import cls_print, check_csv_filename
+# Variables for collected data
+village_list=[]
+region_header_list = []
+region_data_list = []
 
-URL = "https://volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=2&xnumnuts=2102"
-URL_V = "https://volby.cz/pls/ps2017nss/ps311?xjazyk=CZ&xkraj=2&xobec=599417&xvyber=2102"
-CSV_FILE = "vysledky_beroun"
-EMPTY_HTML = '<html> </html>'
-VILLAGE_URL_BASE = 'https://volby.cz/pls/ps2017nss/ps311?xjazyk=CZ&xkraj=2&xobec='
-REGION, VILLAGE = 0, 1
-SELECTED_COLUMNS = ['ČÍSLO','OBEC','POČET VOLIČŮ','VYDANÉ OBÁLKY','PLATNÉ HLASY']
+# Election Scraper
+cls_print("Election Scraper")
+# Arguments check
+if not check_input_arguments(sys.argv):
+    print("- terminating now...")
+    sys.exit()
+else:
+    # Get the list of villages for specified region
+    village_list = get_villages_list(sys.argv[1])
 
-def get_region_number(url : str) -> str:
-    """ Extract the number of the region from URL """
-    return url[url.index('numnuts')+8:]
-
-def save_html_data(content : str, org_code: str) -> None:
-    """ Save the data from the webpage into file for next """
-    with open('z_'+org_code+'_data.html', mode='w', encoding='UTF-8') as f:
-        f.write(content)
-
-def read_html_data(org_code: int) -> str:
-    """ Reads saved html data """
-    with open('z_'+org_code+'_data.html', mode='r', encoding='UTF-8') as f:
-        return f.read()
-
-def get_village_url(election_region_code: str, election_village_code: str) -> str:
-    """ Compose the URL with election results for specific village"""
-    return f"{VILLAGE_URL_BASE}{election_village_code}&xvyber={election_region_code}"
-
-def get_html_data(url: str, org_entity: str) -> str:
-    """ Downloads the requested web page (if not done already) """
-    # Send a GET request if it is first time
-    if os.path.isfile('z_'+org_entity+'_data.html'):
-        # print("- data file exists")
-        html_data = read_html_data(org_entity)
-    else:
-        print("- data file does not exists")
-        try:
-            response = requests.get(url, timeout=5)
-            if response.status_code == 200:
-                # print("- GET request successful")
-                save_html_data(response.text, org_entity)
-                html_data = response.text
-            else:
-                # print("- GET request failed")
-                html_data = EMPTY_HTML
-        except requests.exceptions.Timeout:
-            # print("- GET request timed out")
-            html_data = EMPTY_HTML
-    return html_data
-
-def get_villages_list(page_tables) -> []:
-    """ Get full list of vilages as dict """
-    result_list = []
-    # Get data from all tables on page
-    for table in (page_tables):
-        for rows in table.find_all('tr'):
-            rows_data = [td.text for td in rows.find_all('td')]
-            if len(rows_data):
-                if rows_data[0].isnumeric():
-                    result_list.append([rows_data[0], rows_data[1]])
-    return result_list
-
-def get_village_results(village_code: str, village_name: str, page_tables):
-    """ Function is returning two lists - list of headers and list of results for specific village """
-    # Pickup data from tables
-    village_general_data = [] # Number of voters, envelopes issued, valid votes - 3, 4, 7
-    list_of_parties = []
-    votes_received = []
-    for counter, table in enumerate(page_tables):
-        for row in table.find_all('tr'):
-            if row.find('td'):
-                row_data = [td.text.replace('\xa0','').replace(',','.') for td in row.find_all('td')]
-                if not counter: # First table is including general data
-                    village_general_data= [row_data[3], row_data[4], row_data[7]]
-                else: # Other tables contains results of parties
-                    list_of_parties.append(row_data[1])
-                    votes_received.append(row_data[2])
-    headers_list = SELECTED_COLUMNS + list_of_parties
-    data_list = [village_code, village_name]+village_general_data + votes_received
-    return headers_list, data_list
-
-# What region we are looking for?
-REGION_NO = get_region_number(URL)
-# Parse the HTML content of the page with BeautifulSoup
-soup = BeautifulSoup(get_html_data(URL, REGION_NO), 'html.parser')
-village_list = get_villages_list(soup.find_all('table'))
-header_list = []
-data_list = []
-
-# Get the election results from all villages in the region
-for cnt, village in enumerate(village_list):
+for village in village_list:
     # Compose link to data for specific village
-    village_code, village_name = village
-    village_url = get_village_url(REGION_NO, village_code)
-    # Parse the page and get table(s)
-    soup = BeautifulSoup(get_html_data(village_url, village_code), 'html.parser')
-    table_tabs = soup.find_all('table')
+    village_code, village_name, village_link = village
+    village_url = get_village_url(village_link)
+    # Print the processed village name and document progress
+    print("-", village_name)
     # Get list of headers and list of data from the tables
-    v_header, v_data = get_village_results(village_code, village_name, table_tabs)
-    # Get Header if it is first time
-    if not cnt:
-        header_list = v_header
-    data_list.append(v_data)
+    region_header_list, village_data = get_village_results(village_url)
+    # Update data list with village code and name as first two items
+    village_data.insert(0, village_name)
+    village_data.insert(0, village_code)
+    region_data_list.append(village_data)
 
 # Save to CSV using pandas
-df = pd.DataFrame(data_list, columns=header_list)
-# Save the DataFrame as a CSV file
-df.to_csv('#output.csv', index=False)
-
-
-
+csv_file = check_csv_filename(sys.argv[2])
+df = pd.DataFrame(region_data_list, columns=region_header_list)
+df.to_csv(csv_file, index=False)
+# All done
+cls_print("Election Scraper")
+print("- collected data saved to", csv_file)
+print("- scraping finished")
